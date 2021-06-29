71906cf6e1c3154f8ef098cd43194a0dce5dace2
akshay <you@example.com>
2021-06-29 14:16:32 +0530

    iwa website

---
+++ js/script.js
@@ -0,0 +1,39 @@
+const root = document.querySelector("body");
+
+// Real cursor element
+const cursor = document.createElement("div");
+cursor.classList.add("cursor");
+cursor.setAttribute("data-scale", "1");
+root.appendChild(cursor);
+
+// // Following extra cursor element
+// const follower = document.createElement('div');
+// follower.classList.add('cursor', 'cursor__follower');
+// root.appendChild(follower);
+
+window.addEventListener("mousemove", (e) => {
+  //   setPosition(follower, e);
+  setPosition(cursor, e);
+});
+// window.addEventListener("scroll", (e) => {
+//   //   setPosition(follower, e);
+//   setPosition(cursor, e);
+// });
+
+function setPosition(element, e) {
+    element.setAttribute("style", `top: ${(e.pageY-7.5)}px; left: ${(e.pageX-7.5)}px;`)
+  //   element.style.transform = `translate3d(${e.clientX}px, ${e.clientY}px, 0)`;
+//   element.style.transform = `translate3d(${e.pageX}px, ${e.pageY}px, 0) scale(${cursor.getAttribute("data-scale")}) `;
+}
+
+document.querySelectorAll("a, button, .faqs-item, .button").forEach((elem) => {
+  elem.addEventListener("mouseenter", () =>{
+    cursor.classList.add("cursor-hover");
+    cursor.setAttribute("data-scale", "2.5");
+  });
+  elem.addEventListener("mouseleave", () =>{
+    cursor.classList.remove("cursor-hover");
+    
+    cursor.setAttribute("data-scale", "1");
+  });
+});
